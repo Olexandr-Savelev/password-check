c4887bf78ed9e6753bbb0fccf308679e374a63c3
Oleksandr Savelev <a.savelev.krm@gmail.com>
2024-02-14 21:22:56 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'password-check';
+
+  passwordValue: string = '';
+
+  isHide: boolean = true;
+
+  togglePasswordVisibility() {
+    this.isHide = !this.isHide;
+  }
+
+  onPasswordChange(event: Event): void {
+    const { target } = event;
+    this.passwordValue = (target as HTMLInputElement).value;
+  }
 }
---
+++ src/app/indicator/indicator.component.ts
@@ -0,0 +1,72 @@
+import { Component, Input, OnChanges } from '@angular/core';
+
+@Component({
+  selector: 'app-indicator',
+  templateUrl: './indicator.component.html',
+  styleUrls: ['./indicator.component.scss'],
+})
+export class IndicatorComponent implements OnChanges {
+  @Input() indexNumber!: number;
+  @Input() passwordValue: string = '';
+
+  isEmpty: boolean = true;
+  isLongEnought: boolean = false;
+
+  isEasy: boolean = false;
+  isMedium: boolean = false;
+  isStrong: boolean = false;
+
+  checkPasswordCharacters(value: string) {
+    const digitsPattern = /\d/;
+    const lettersPattern = /[a-zA-Z]/;
+    const symbolsPattern = /[^a-zA-Z0-9]/;
+
+    const containDigits = digitsPattern.test(value);
+    const containLetters = lettersPattern.test(value);
+    const containSymbols = symbolsPattern.test(value);
+
+    return {
+      containDigits,
+      containLetters,
+      containSymbols,
+    };
+  }
+
+  ngOnChanges(): void {
+    if (this.passwordValue.length !== 0) {
+      this.isEmpty = false;
+    } else {
+      this.isEmpty = true;
+    }
+
+    if (this.passwordValue.length >= 8) {
+      this.isLongEnought = true;
+
+      const { containDigits, containLetters, containSymbols } =
+        this.checkPasswordCharacters(this.passwordValue);
+
+      if (containDigits && containLetters && containSymbols) {
+        this.isStrong = true;
+        this.isMedium = false;
+        this.isEasy = false;
+      } else if (
+        (containDigits && containLetters) ||
+        (containLetters && containSymbols) ||
+        (containDigits && containSymbols)
+      ) {
+        this.isStrong = false;
+        this.isMedium = true;
+        this.isEasy = false;
+      } else {
+        this.isStrong = false;
+        this.isMedium = false;
+        this.isEasy = true;
+      }
+    } else {
+      this.isLongEnought = false;
+      this.isStrong = false;
+      this.isMedium = false;
+      this.isEasy = false;
+    }
+  }
+}
