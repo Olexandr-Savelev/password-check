1aa71c3bfdf4f655c58fe5afb1cbad98b7eb7575
Oleksandr Savelev <a.savelev.krm@gmail.com>
2024-02-18 00:58:51 +0200

    replace logic from indicators to parent component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,9 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'password-check';
 
-  passwordValue: string = '';
+  isEmpty: boolean = true;
+  isLongEnough: boolean = false;
+  passwordStrength: string = '';
 
   isHide: boolean = true;
 
@@ -16,8 +18,54 @@ export class AppComponent {
     this.isHide = !this.isHide;
   }
 
+  checkPasswordCharacters(value: string) {
+    const digitsPattern = /\d/;
+    const lettersPattern = /[a-zA-Z]/;
+    const symbolsPattern = /[^a-zA-Z0-9]/;
+
+    const containDigits = digitsPattern.test(value);
+    const containLetters = lettersPattern.test(value);
+    const containSymbols = symbolsPattern.test(value);
+
+    return {
+      containDigits,
+      containLetters,
+      containSymbols,
+    };
+  }
+
   onPasswordChange(event: Event): void {
     const { target } = event;
-    this.passwordValue = (target as HTMLInputElement).value;
+    const passwordValue = (target as HTMLInputElement).value;
+
+    console.log(passwordValue.length);
+
+    if (passwordValue.length !== 0) {
+      this.isEmpty = false;
+    } else {
+      this.isEmpty = true;
+    }
+
+    if (passwordValue.length >= 8) {
+      this.isLongEnough = true;
+
+      const { containDigits, containLetters, containSymbols } =
+        this.checkPasswordCharacters(passwordValue);
+
+      if (containDigits && containLetters && containSymbols) {
+        this.passwordStrength = 'Strong';
+      } else if (
+        (containDigits && containLetters) ||
+        (containLetters && containSymbols) ||
+        (containDigits && containSymbols)
+      ) {
+        this.passwordStrength = 'Medium';
+      } else {
+        this.passwordStrength = 'Easy';
+      }
+    } else {
+      this.isLongEnough = false;
+      this.passwordStrength = '';
+    }
   }
 }
--- src/app/indicator/indicator.component.ts
+++ src/app/indicator/indicator.component.ts
@@ -1,72 +1,13 @@
-import { Component, Input, OnChanges } from '@angular/core';
+import { Component, Input, OnChanges, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-indicator',
   templateUrl: './indicator.component.html',
   styleUrls: ['./indicator.component.scss'],
 })
-export class IndicatorComponent implements OnChanges {
+export class IndicatorComponent {
   @Input() id!: number;
-  @Input() passwordValue: string = '';
-
-  isEmpty: boolean = true;
-  isLongEnought: boolean = false;
-
-  isEasy: boolean = false;
-  isMedium: boolean = false;
-  isStrong: boolean = false;
-
-  checkPasswordCharacters(value: string) {
-    const digitsPattern = /\d/;
-    const lettersPattern = /[a-zA-Z]/;
-    const symbolsPattern = /[^a-zA-Z0-9]/;
-
-    const containDigits = digitsPattern.test(value);
-    const containLetters = lettersPattern.test(value);
-    const containSymbols = symbolsPattern.test(value);
-
-    return {
-      containDigits,
-      containLetters,
-      containSymbols,
-    };
-  }
-
-  ngOnChanges(): void {
-    if (this.passwordValue.length !== 0) {
-      this.isEmpty = false;
-    } else {
-      this.isEmpty = true;
-    }
-
-    if (this.passwordValue.length >= 8) {
-      this.isLongEnought = true;
-
-      const { containDigits, containLetters, containSymbols } =
-        this.checkPasswordCharacters(this.passwordValue);
-
-      if (containDigits && containLetters && containSymbols) {
-        this.isStrong = true;
-        this.isMedium = false;
-        this.isEasy = false;
-      } else if (
-        (containDigits && containLetters) ||
-        (containLetters && containSymbols) ||
-        (containDigits && containSymbols)
-      ) {
-        this.isStrong = false;
-        this.isMedium = true;
-        this.isEasy = false;
-      } else {
-        this.isStrong = false;
-        this.isMedium = false;
-        this.isEasy = true;
-      }
-    } else {
-      this.isLongEnought = false;
-      this.isStrong = false;
-      this.isMedium = false;
-      this.isEasy = false;
-    }
-  }
+  @Input() passwordStrength!: string;
+  @Input() isLongEnough!: boolean;
+  @Input() isEmpty!: boolean;
 }
